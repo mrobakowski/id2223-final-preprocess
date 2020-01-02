1a8525e936305fd06eb3ae8c8b1f4ad430f656a7
mrobakowski <mikolaj.rob@gmail.com>
2020-01-02 18:11:29 +0100

    process the test regions

--- src/main/scala/se/kth/id2223/finalproject/Main.scala
+++ src/main/scala/se/kth/id2223/finalproject/Main.scala
@@ -55,18 +55,29 @@ object Properties {
   implicit val decoder: Decoder[Properties] = Decoder.forProduct3("verified", "roof_material", "id")(Properties.apply)
 }
 
+case class TestProperties(id: String)
+
+object TestProperties {
+  implicit val decoder: Decoder[TestProperties] = Decoder.forProduct1("id")(TestProperties.apply)
+}
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val (input, output) = args match {
-      case Array(input, output) => (input, output)
-      case Array(input) => (input, "output")
-      case Array() => ("stac-data/stac", "output")
+    // arg parsing
+    val (input, output, testOutput) = args match {
+      case Array(input, output, testOutput) => (input, output, testOutput)
+      case Array(input, output) => (input, output, "test-output")
+      case Array(input) => (input, "output", "test-output")
+      case Array() => ("stac-data/stac", "output", "test-output")
     }
 
+    // create output directories
     RoofMaterial.ALL.foreach { rm =>
       Files.createDirectories(Paths.get(output, rm.toString))
     }
+    Files.createDirectories(Paths.get(testOutput))
 
+    // crawl the input folder for the tiff and geojson files
     Files.walkFileTree(Paths.get(input), new SimpleFileVisitor[Path] {
       override def preVisitDirectory(dir: Path, attrs: BasicFileAttributes): FileVisitResult = {
         val pathsInDir = Files.list(dir).iterator().asScala.toList
@@ -74,11 +85,14 @@ object Main {
         val trainGeoJsonFile = pathsInDir.collectFirst {
           case path if path.getFileName.toString.startsWith("train-") && path.toString.endsWith(".geojson") => path
         }
+        val testGeoJsonFile = pathsInDir.collectFirst {
+          case path if path.getFileName.toString.startsWith("test-") && path.toString.endsWith(".geojson") => path
+        }.map(_.toString)
 
         var found = false
 
         tiffFile.zip(trainGeoJsonFile).foreach { case (tiff, geoJson) =>
-          processGeoTiff(tiff.toString, geoJson.toString, output)
+          processGeoTiff(tiff.toString, geoJson.toString, testGeoJsonFile, output, testOutput)
           found = true
         }
 
@@ -87,7 +101,7 @@ object Main {
     })
   }
 
-  def processGeoTiff(imagePath: String, geoJsonPath: String, outputDirectory: String): Unit = {
+  def processGeoTiff(imagePath: String, geoJsonPath: String, testGeoJsonPath: Option[String], outputDirectory: String, testOutputDirectory: String): Unit = {
     val dirName = Paths.get(imagePath).getParent.toString
 
     // streaming GeoTiffs are not thread-safe, so each thread gets its own copy
@@ -98,21 +112,50 @@ object Main {
     println(f"processing $dirName")
     val pb = new ProgressBar(f"extracting features", features.size)
 
+    // .par means the processing is done on all the processor cores
     features.par.foreach { feature =>
       saveFeatureAsSeparatePng(feature, image.get(), outputDirectory)
       pb.step()
     }
 
     pb.close()
+
+    // if there are any test geojsons, extract the features from them as well
+    testGeoJsonPath.foreach { testPath =>
+      val json = GeoJson.fromFile[JsonFeatureCollection](testPath)
+      val features = json.getAllFeatures[Feature[Geometry, TestProperties]]
+      println(f"processing test $dirName")
+      val pb = new ProgressBar(f"extracting features", features.size)
+
+      // .par means the processing is done on all the processor cores
+      features.par.foreach { feature =>
+        saveTestFeatureAsSeparatePng(feature, image.get(), testOutputDirectory)
+        pb.step()
+      }
+
+      pb.close()
+    }
+
+  }
+
+  def cropFeature(feature: Feature[Geometry, _], image: MultibandGeoTiff): MultibandGeoTiff = {
+    val extentInFeatureCoords: Extent = feature.geom.getEnvelopeInternal
+    // the coordinate system of the features is geographical latitude and longitude
+    val extentInImageCoords = extentInFeatureCoords.reproject(LatLng, image.crs)
+    image.crop(extentInImageCoords)
   }
 
   def saveFeatureAsSeparatePng(feature: Feature[Geometry, Properties], image: MultibandGeoTiff, outputDirectory: String): Unit = {
     val Properties(_, material, id) = feature.data
-    val extentInFeatureCoords: Extent = feature.geom.getEnvelopeInternal
-    // the coordinate system of the features is geographical latitude and longitude
-    val extentInImageCoords = extentInFeatureCoords.reproject(LatLng, image.crs)
-    val croppedImage = image.crop(extentInImageCoords)
+    val croppedImage = cropFeature(feature, image)
 
     croppedImage.tile.renderPng().write(s"$outputDirectory/$material/$id.png")
+  }
+
+  def saveTestFeatureAsSeparatePng(feature: Feature[Geometry, TestProperties], image: MultibandGeoTiff, outputDirectory: String): Unit = {
+    val TestProperties(id) = feature.data
+    val croppedImage = cropFeature(feature, image)
+
+    croppedImage.tile.renderPng().write(s"$outputDirectory/$id.png")
   }
 }
